32eae6c40635399f8a23c61a2448b1922e1bc347
Janvi Bhatt <jb.janvibhatt@gmail.com>
2022-05-27 13:39:13 +0530

    Item's recyclerView added

--- app/src/main/java/com/example/groceryapp/MainActivity.kt
+++ app/src/main/java/com/example/groceryapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.groceryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var itemsRv: RecyclerView
+    lateinit var itemsFb: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        itemsRv = findViewById(R.id.id_RView)
+        itemsFb = findViewById(R.id.id_FButton)
+
     }
 }
